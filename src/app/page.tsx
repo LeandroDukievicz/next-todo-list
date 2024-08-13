814d3782c02f61c31195dbd330053f3e9ce9ac0b
LeandroDukievicz <ldukie@icloud.com>
2024-08-13 16:55:31 -0300

    add feat

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,31 @@
 import Image from "next/image";
-import styles from "./page.module.css";
+import Head from "next/head";
+import styles from "../../styles/page.module.css"
 
+import todoProject from "../../public/assets/todoProject.png"
 
-const Home = () => {
-  return (
-    <div>
-      Hello World
-    </div>
+export default function Home(){
+  return(
+    <section className={styles.container}>
+      <Head>
+        <title>Projeto To Do List - Organizando tarefas de forma fácil
+        </title>
+      </Head>
+
+      <main className={styles.main}>
+        <div className={styles.logoContent}>
+          <Image className={styles.todoProject}
+                  alt="Logo Projeto to do list"
+                  src={todoProject}
+                  priority 
+          />
+
+          <h1 className={styles.title}>
+            Projeto simples para você organizar <br /> Suas tarefas e estudos !
+          </h1>
+          
+        </div>
+      </main>
+    </section>
   )
 }
-
-export default Home
