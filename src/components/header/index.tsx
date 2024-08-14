8a64b8475b67994d85ed6076033ec22d7253090b
LeandroDukievicz <ldukie@icloud.com>
2024-08-14 12:42:17 -0300

    📦 FEAT: add navbar and stylesheet from navbar

---
+++ src/components/header/index.tsx
@@ -0,0 +1,29 @@
+import Link from 'next/link'
+
+import styles from "./style.module.css";
+
+export function Header(){
+    return(
+        <header className={styles.header}>
+            <section  className={styles.content}>
+                <nav className={styles.nav}>
+                    <Link href="/">
+                        <h1 className={styles.logo}>To Do
+                            <span>+</span>
+                        </h1>
+                    </Link>
+                    
+                    <Link className={styles.myPanel} 
+                                    href="/dashboard">
+                                    Meu Painel
+                    </Link>
+                
+                </nav>
+                
+                <button className={styles.loginButton}>Acessar</button>
+
+            </section>
+        </header>
+    );
+};
+
