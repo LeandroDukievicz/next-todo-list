7b51cc4576ff74eff4d3d918835f6a7b79dcf1d9
LeandroDukievicz <ldukie@icloud.com>
2024-08-23 21:59:42 -0300

    📦 FEAT: added component textarea and create dashboard pash and your style

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -4,7 +4,9 @@ import { getSession } from "next-auth/react";
 import Link from "next/link";
 import styles from "./dashboard.module.css";
 import Head from "next/head";
-import { redirect } from 'next/navigation';
+
+import { Textarea } from "../../components/textArea"
+
 
 
 
@@ -26,7 +28,26 @@ export default async function Dashboard() {
             <Head>
                 <title>Painel de Tarefas</title>
             </Head>
-            <h1>Meu Painel</h1>
+
+            <main className={styles.main}>
+                <section className={styles.content}>
+                    <div className={styles.contentForm}>
+                        <h1 className={styles.title}>   
+                            Qual sua Tarefa?
+                        </h1>
+                        <form>
+                            <Textarea placeholder="Digite qual é sua tarefa..."/>
+                            <div className = {styles.checkboxArea}>
+                                <input type="checkbox" className={styles.checkbox} />
+                                <label>Deixar esta tarefa pública?</label>
+                            </div>
+                            <button className={styles.button} type="submit">
+                                Registrar Tarefa
+                            </button>
+                        </form>
+                    </div>
+                </section>
+            </main>
         </div>
     );
 }
