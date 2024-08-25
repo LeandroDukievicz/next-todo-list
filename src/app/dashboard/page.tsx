79e646579f34a225efa8cb454b0dd59ca0641a29
LeandroDukievicz <ldukie@icloud.com>
2024-08-24 22:54:34 -0300

    📝 CONTENT: modified dashboard page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,86 +1,113 @@
 "use client";
 
-import { getSession, useSession } from "next-auth/react";
-import Link from "next/link";
+import { useSession } from "next-auth/react";
 import { ChangeEvent, FormEvent, useState, useEffect } from "react";
-import styles from "./dashboard.module.css";
+import styles from "./styles.module.css";
 import Head from "next/head";
-import { Textarea } from "../../components/textArea";
+import { Textarea } from "../../components/textarea";
 import { FiShare2 } from "react-icons/fi";
 import { FaTrash } from "react-icons/fa";
+import { db } from "../../services/firebaseConnection"; // Importando a conexão com o Firebase
+import { addDoc, collection } from "firebase/firestore";
 
 export default function Dashboard() {
-    const { data: session, status } = useSession();
-    const [input, setInput] = useState("");
-    const [publicTask, setPublicTask] = useState(false);
+  const { data: session, status } = useSession();
+  const [input, setInput] = useState("");
+  const [publicTask, setPublicTask] = useState(false);
 
-    function handleChangePublic(event: ChangeEvent<HTMLInputElement>) {
-        setPublicTask(event.target.checked);
+  useEffect(() => {
+    if (status === "unauthenticated") {
+      window.location.href = "/";
     }
+  }, [status]);
 
-    function handleRegisterTask(event: FormEvent){
-        event.preventDefault();
-        
+  function handleChangePublic(event: ChangeEvent<HTMLInputElement>) {
+    setPublicTask(event.target.checked);
+  }
 
+  async function handleRegisterTask(event: FormEvent) {
+    event.preventDefault();
+
+    if (input === "") return;
+
+    try {
+      console.log("Attempting to add document with:", {
+        tarefa: input,
+        created: new Date(),
+        user: session?.user?.email,
+        public: publicTask,
+      });
+
+      await addDoc(collection(db, "tarefas"), {
+        tarefa: input,
+        created: new Date(),
+        user: session?.user?.email,
+        public: publicTask,
+      });
+
+      setInput("");
+      setPublicTask(false);
+    } catch (err) {
+      console.error("Error adding document: ", err);
     }
+  }
 
-    if (status === "loading") {
-        return <p>Carregando...</p>;
-    }
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Meu painel de tarefas</title>
+      </Head>
 
-    return (
-        <div className={styles.container}>
-            <Head>
-                <title>Painel de Tarefas</title>
-            </Head>
-            <main className={styles.main}>
-                <section className={styles.content}>
-                    <div className={styles.contentForm}>
-                        <h1 className={styles.title}>Qual sua Tarefa?</h1>
+      <main className={styles.main}>
+        <section className={styles.content}>
+          <div className={styles.contentForm}>
+            <h1 className={styles.title}>Qual sua tarefa?</h1>
 
-                        <form onSubmit={handleRegisterTask}>
-                            <Textarea
-                                placeholder="Digite qual é sua tarefa..."
-                                value={input}
-                                onChange={(event: ChangeEvent<HTMLTextAreaElement>) =>
-                                    setInput(event.target.value)
-                                }
-                            />
+            <form onSubmit={handleRegisterTask}>
+              <Textarea
+                placeholder="Digite qual sua tarefa..."
+                value={input}
+                onChange={(event: ChangeEvent<HTMLTextAreaElement>) =>
+                  setInput(event.target.value)
+                }
+              />
+              <div className={styles.checkboxArea}>
+                <input
+                  type="checkbox"
+                  className={styles.checkbox}
+                  checked={publicTask}
+                  onChange={handleChangePublic}
+                />
+                <label>Deixar tarefa pública?</label>
+              </div>
 
-                            <div className={styles.checkboxArea}>
-                                <input
-                                    type="checkbox"
-                                    className={styles.checkbox}
-                                    checked={publicTask}
-                                    onChange={handleChangePublic}
-                                />
-                                <label>Deixar esta tarefa pública?</label>
-                            </div>
-                            <button className={styles.button} type="submit">
-                                Registrar Tarefa
-                            </button>
-                        </form>
-                    </div>
-                </section>
+              <button className={styles.button} type="submit">
+                Registrar
+              </button>
+            </form>
+          </div>
+        </section>
 
-                <section className={styles.taskContainer}>
-                    <h1>Minhas Tarefas</h1>
-                    <article className={styles.task}>
-                        <div className={styles.tagContainer}>
-                            <label className={styles.tag}>PÚBLICO</label>
-                            <button className={styles.shareButton}>
-                                <FiShare2 size={23} color="#6463e3" />
-                            </button>
-                        </div>
-                        <div className={styles.taskContent}>
-                            <p></p>
-                            <button className={styles.trashButton}>
-                                <FaTrash size={24} color="#ea3140" />
-                            </button>
-                        </div>
-                    </article>
-                </section>
-            </main>
-        </div>
-    );
+        <section className={styles.taskContainer}>
+          <h1>Minhas tarefas</h1>
+
+          <article className={styles.task}>
+            <div className={styles.tagContainer}>
+              <label className={styles.tag}>PÚBLICO</label>
+              <button className={styles.shareButton}>
+                <FiShare2 size={22} color="#3183ff" />
+              </button>
+            </div>
+
+            <div className={styles.taskContent}>
+              <p>Minha primeira tarefa de exemplo, show demais!</p>
+              <button className={styles.trashButton}>
+                <FaTrash size={24} color="#ea3140" />
+              </button>
+            </div>
+          </article>
+        </section>
+      </main>
+    </div>
+  );
 }
--- src/services/firebaseConnection.ts
+++ src/services/firebaseConnection.ts
@@ -1,6 +1,5 @@
-
 import { initializeApp } from "firebase/app";
-import { getFirestore } from 'firebase/firestore'
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAfo1wZKbNQ01xqOeFo28vcuv-pbXD7ZQ4",
@@ -11,9 +10,7 @@ const firebaseConfig = {
   appId: "1:956548769891:web:3ade89d642b819e28d7056"
 };
 
-
 const firebaseApp = initializeApp(firebaseConfig);
-
-const db = getFirestore(fireabaseApp)
+const db = getFirestore(firebaseApp);
 
 export { db };
